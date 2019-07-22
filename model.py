b469bd89c6fc30999c1a0bcae5d1f17a341a5823
Mark Wells <wellowdata@gmail.com>
2019-07-22 14:53:25 +0000

    first commit

--- model.py
+++ model.py
@@ -1,7 +1,179 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
+
+
+
+class LayoutNet(nn.Module):
+    def __init__(self):
+        super(LayoutNet, self).__init__()
+        self.enc1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.enc2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.enc3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1), 
+            nn.ReLU(inplace=True)
+        )
+
+        self.enc4 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.enc5 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.enc6 = nn.Sequential(
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.enc7 = nn.Sequential(
+            nn.Conv2d(1024, 2048, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )        
+
+        self.dec1 = nn.Sequential(
+            nn.Conv2d(2048, 1024, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.dec2 = nn.Sequential(
+            nn.Conv2d(1024*2, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.dec3 = nn.Sequential(
+            nn.Conv2d(512*2, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.dec4 = nn.Sequential(
+            nn.Conv2d(256*2, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.dec5 = nn.Sequential(
+            nn.Conv2d(128*2, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )        
+        self.dec6 = nn.Sequential(
+            nn.Conv2d(64*2, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )        
+        self.dec7 = nn.Sequential(
+            nn.Conv2d(32*2, out_planes, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )        
+
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+      
+        
+        
+    def forward(self, x):
+        ## ENCODER
+        encoder1 = self.enc1(x)
+        encoder1_ds = self.pool(encoder1)
+        encoder2 = self.enc2(encoder1_ds)
+        encoder2_ds = self.pool(encoder2)
+        encoder3 = self.enc3(encoder2_ds)
+        encoder3_ds = self.pool(encoder3)
+        encoder4 = self.enc4(encoder3_ds)
+        encoder4_ds = self.pool(encoder4)
+        encoder5 = self.enc5(encoder4_ds)
+        encoder5_ds = self.pool(encoder5)
+        encoder6 = self.enc6(encoder5_ds)
+        encoder6_ds = self.pool(encoder6)
+        encoder7 = self.enc7(encoder6_ds)
+        encoder7_ds = self.pool(encoder7)
+
+
+
+        ## DECODER
+
+        decoder1 = self.dec1(torch.cat([F.interpolate(encoder7, 2), decoder?]))
+        decoder2 = self.dec2(torch.cat([F.interpolate(encoder2, 2), decoder2]))
+        decoder3 = self.dec3(torch.cat([F.interpolate(encoder3, 2), decoder3]))
+        decoder4 = self.dec4(torch.cat([F.interpolate(encoder4, 2), decoder4]))
+        decoder5 = self.dec5(torch.cat([F.interpolate(encoder5, 2), decoder5]))
+        decoder6 = self.dec6(torch.cat([F.interpolate(encoder6, 2), decoder6]))
+        decoder6_sigmoid = 
+
+local unpool00 = nn.SpatialUpSamplingNearest(2)(pool7)
+local deconv00 = nn.SpatialConvolution(2048,1024,3,3,1,1,1,1)(unpool00)
+local deconv00_relu = nn.ReLU(true)(deconv00)
+
+local unpool0_ = nn.JoinTable(2)({deconv00_relu, pool6})
+
+local unpool0 = nn.SpatialUpSamplingNearest(2)(unpool0_)
+local deconv0 = nn.SpatialConvolution(1024*2,512,3,3,1,1,1,1)(unpool0)
+local deconv0_relu = nn.ReLU(true)(deconv0)
+
+local unpool1_ = nn.JoinTable(2)({deconv0_relu, pool5})
+
+local unpool1 = nn.SpatialUpSamplingNearest(2)(unpool1_)
+local deconv1 = nn.SpatialConvolution(512*2,256,3,3,1,1,1,1)(unpool1)
+local deconv1_relu = nn.ReLU(true)(deconv1)
+
+local unpool2_ = nn.JoinTable(2)({deconv1_relu, pool4})
+
+local unpool2 = nn.SpatialUpSamplingNearest(2)(unpool2_)
+local deconv2 = nn.SpatialConvolution(256*2,128,3,3,1,1,1,1)(unpool2)
+local deconv2_relu = nn.ReLU(true)(deconv2)
+
+local unpool3_ = nn.JoinTable(2)({deconv2_relu, pool3})
+
+local unpool3 = nn.SpatialUpSamplingNearest(2)(unpool3_)
+local deconv3 = nn.SpatialConvolution(128*2,64,3,3,1,1,1,1)(unpool3)
+local deconv3_relu = nn.ReLU(true)(deconv3)
+
+local unpool4_ = nn.JoinTable(2)({deconv3_relu, pool2})
+
+local unpool4 = nn.SpatialUpSamplingNearest(2)(unpool4_)
+local deconv4 = nn.SpatialConvolution(64*2,32,3,3,1,1,1,1)(unpool4)
+local deconv4_relu = nn.ReLU(true)(deconv4)
+
+local unpool5_ = nn.JoinTable(2)({deconv4_relu, pool1})
+
+local unpool5 = nn.SpatialUpSamplingNearest(2)(unpool5_)
+local deconv5 = nn.SpatialConvolution(32*2,3,3,3,1,1,1,1)(unpool5)
+local deconv6_sf = nn.Sigmoid()(deconv5)        
+        
+'''
+unpool00 = nn.SpatialUpSamplingNearest(2)(pool7)
+deconv00 = nn.SpatialConvolution(2048,1024,3,3,1,1,1,1)(unpool00)
+deconv00_relu = nn.ReLU(true)(deconv00)
+'''
+        ## REFINEMENT
+
+        ref0 = nn.reshape(2048 * 4 * 4) #(pool7)
+        ref1 = nn.Linear(2048 * 4 * 4, 1024) #(ref0)
+        ref1_relu = nn.ReLU(inplace=True) #(ref1)
+        ref2 = nn.Linear(1024, 256) #(ref1_relu)
+        ref2_relu = nn.ReLU(inplace=True) #(ref2)
+        ref3 = nn.Linear(256, 64) #(ref2_relu)
+        ref3_relu = nn.ReLU(inplace=True) #(ref3)
+        roomtype = nn.Linear(64, 11) #(ref3_relu)
+
+
+
+
+
+        return decoder6_sigmoid, decoder6_sigmoid_corners, roomtype
+
+
+
+
+
+
+
+
+
+
+
 
 
 def conv3x3(in_planes, out_planes):
@@ -61,6 +233,25 @@ class Decoder(nn.Module):
             f_last, scale_factor=2, mode='nearest')))
         return conv_out
 
+    
+class Refinement(nn.Module):
+    def __init__(self, out_cat=11):  #or 8??
+        super(Refinement, self).__init__()
+        'torch.reshape(-1) #flatten(2048*4*4)'
+        self.refine = nn.sequential([
+            nn.linear(2048*4*4, 1024),
+            nn.ReLU(inplace=True),
+            nn.linear(1024, 256),
+            nn.ReLU(inplace=True),
+            nn.linear(256, 64),
+            nn.ReLU(inplace=True),
+            nn.linear(64, out_cat),
+            nn.ReLU(inplace=True),
+            ])
+
+    def forward(self, x):
+        # flatten here?
+        return self.refine(x)
 
 if __name__ == '__main__':
 
