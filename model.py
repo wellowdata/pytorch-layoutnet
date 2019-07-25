4baefc667bded05894c24faaacf1424f70d9bf3f
Mark Wells <wellowdata@gmail.com>
2019-07-25 16:11:59 +0000

    from tensorflow-3

--- model.py
+++ model.py
@@ -3,7 +3,185 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class LayoutNet(nn.Module):
+    def __init__(self):
+        super(LayoutNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1, stride=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1, stride=1)
+        self.conv5 = nn.Conv2d(256, 512, kernel_size=3, padding=1, stride=1)
+        self.conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=1, stride=1)
+        self.conv7 = nn.Conv2d(1024, 2048, kernel_size=3, padding=1, stride=1)
+        self.deconv00 = nn.Conv2d(2048, 1024, kernel_size=3, padding=1, stride=1)
+        self.deconv0 = nn.Conv2d(1024*2, 512, kernel_size=3, padding=1, stride=1)
+        self.deconv1 = nn.Conv2d(512*2, 256, kernel_size=3, padding=1, stride=1)
+        self.deconv2 = nn.Conv2d(256*2, 128, kernel_size=3, padding=1, stride=1)
+        self.deconv3 = nn.Conv2d(128*2, 64, kernel_size=3, padding=1, stride=1)
+        self.deconv4 = nn.Conv2d(64*2, 32, kernel_size=3, padding=1, stride=1)
+        self.deconv5 = nn.Conv2d(32*2, 3, kernel_size=3, padding=1, stride=1)
+        self.deconv6_sf = nn.Sigmoid()
+        self.deconv00_c = nn.Conv2d(2048, 1024, kernel_size=3, padding=1, stride=1)
+        self.deconv0_c = nn.Conv2d(1024*3, 512, kernel_size=3, padding=1, stride=1)
+        self.deconv1_c = nn.Conv2d(512*3, 256, kernel_size=3, padding=1, stride=1)
+        self.deconv2_c = nn.Conv2d(256*3, 128, kernel_size=3, padding=1, stride=1)
+        self.deconv3_c = nn.Conv2d(128*3, 64, kernel_size=3, padding=1, stride=1)
+        self.deconv4_c = nn.Conv2d(64*3, 32, kernel_size=3, padding=1, stride=1)
+        self.deconv5_c = nn.Conv2d(32*3, 16, kernel_size=3, padding=1, stride=1)
+        self.deconv6_sf_c = nn.Sigmoid()
+        self.ref1 = nn.Linear(2048*4*4, 1024)
+        self.ref2 = nn.Linear(1024, 256)
+        self.ref3 = nn.Linear(256, 64)
+        self.ref4 = nn.Linear(64, 11)
+        self.relu = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
+
+    def forward(self, x):
+
+        ## ENCODER
+        conv1 = self.conv1(x)
+        conv1_relu = self.relu(conv1)
+        pool1 = self.pool(conv1_relu)
+        conv2 = self.conv2(pool1)
+        conv2_relu = self.relu(conv2)
+        pool2 = self.pool(conv2_relu)
+        conv3 = self.conv3(pool2)
+        conv3_relu = self.relu(conv3)
+        pool3 = self.pool(conv3_relu)
+        conv4 = self.conv4(pool3)
+        conv4_relu = self.relu(conv4)
+        pool4 = self.pool(conv4_relu)
+        conv5 = self.conv5(pool4)
+        conv5_relu = self.relu(conv5)
+        pool5 = self.pool(conv5_relu)
+        conv6 = self.conv6(pool5)
+        conv6_relu = self.relu(conv6)
+        pool6 = self.pool(conv6_relu)
+        conv7 = self.conv7(pool6)
+        conv7_relu = self.relu(conv7)
+        pool7 = self.pool(conv7_relu)
+
+        ## DECODER
+        unpool00 = F.interpolate(pool7, scale_factor=2)
+        deconv00 = self.deconv00(unpool00)
+        deconv00_relu = self.relu(deconv00)
+        unpool0_ = torch.cat((deconv00_relu, pool6), dim=1)
+        unpool0 = F.interpolate(unpool0_, scale_factor=2)
+        deconv0 = self.deconv0(unpool0)
+        deconv0_relu = self.relu(deconv0)
+        unpool1_ = torch.cat((deconv0_relu, pool5), dim=1)
+        unpool1 = F.interpolate(unpool1_, scale_factor=2)
+        deconv1 = self.deconv1(unpool1)
+        deconv1_relu = self.relu(deconv1)
+        unpool2_ = torch.cat((deconv1_relu, pool4), dim=1)
+        unpool2 = F.interpolate(unpool2_, scale_factor=2)
+        deconv2 = self.deconv2(unpool2)
+        deconv2_relu = self.relu(deconv2)
+        unpool3_ = torch.cat((deconv2_relu, pool3), dim=1)
+        unpool3 = F.interpolate(unpool3_, scale_factor=2)
+        deconv3 = self.deconv3(unpool3)
+        deconv3_relu = self.relu(deconv3)
+        unpool4_ = torch.cat((deconv3_relu, pool2), dim=1)
+        unpool4 = F.interpolate(unpool4_, scale_factor=2)
+        deconv4 = self.deconv4(unpool4)
+        deconv4_relu = self.relu(deconv4)
+        unpool5_ = torch.cat((deconv4_relu, pool1), dim=1)
+        unpool5 = F.interpolate(unpool5_, scale_factor=2)
+        deconv5 = self.deconv5(unpool5)
+        deconv6_sf = self.deconv6_sf(deconv5)
+
+        ## JOINT
+        deconv00_c = self.deconv00_c(unpool00)
+        deconv00_relu_c = self.relu(deconv00_c)
+        unpool0_c = torch.cat((deconv00_relu_c, unpool0_), dim=1)
+        unpool0_c = F.interpolate(unpool0_c, scale_factor=2)
+        deconv0_c = self.deconv0_c(unpool0_c)
+        deconv0_relu_c = self.relu(deconv0_c)
+        unpool1_c = torch.cat((deconv0_relu_c, unpool1_), dim=1)
+        unpool1_c = F.interpolate(unpool1_c, scale_factor=2)
+        deconv1_c = self.deconv1_c(unpool1_c)
+        deconv1_relu_c = self.relu(deconv1_c)
+        unpool2_c = torch.cat((deconv1_relu_c, unpool2_), dim=1)
+        unpool2_c = F.interpolate(unpool2_c, scale_factor=2)
+        deconv2_c = self.deconv2_c(unpool2_c)
+        deconv2_relu_c = self.relu(deconv2_c)
+        unpool3_c = torch.cat((deconv2_relu_c, unpool3_), dim=1)
+        unpool3_c = F.interpolate(unpool3_c, scale_factor=2)
+        deconv3_c = self.deconv3_c(unpool3_c)
+        deconv3_relu_c = self.relu(deconv3_c)
+        unpool4_c = torch.cat((deconv3_relu_c, unpool4_), dim=1)
+        unpool4_c = F.interpolate(unpool4_c, scale_factor=2)
+        deconv4_c = self.deconv4_c(unpool4_c)
+        deconv4_relu_c = self.relu(deconv4_c)
+        unpool5_c = torch.cat((deconv4_relu_c, unpool5_), dim=1)
+        unpool5_c = F.interpolate(unpool5_c, scale_factor=2)
+        deconv5_c = self.deconv5_c(unpool5_c)
+        deconv6_sf_c = self.deconv6_sf_c(deconv5_c)
+
+        ## REFINEMENT
+        ref0 = pool7.view(-1, 2048*4*4)
+        ref1 = self.ref1(ref0)
+        ref1_relu = self.relu(ref1)
+        ref2 = self.ref2(ref1_relu)
+        ref2_relu = self.relu(ref2)
+        ref3 = self.ref3(ref2_relu)
+        ref3_relu = self.relu(ref3)
+        ref4 = self.ref4(ref3_relu)
+
+        return deconv6_sf, deconv6_sf_c, ref4
+
+
+if __name__ == '__main__':
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    model = LayoutNet()
+    print(count_parameters(model))
+    x = torch.rand(2, 3, 512, 512)
+    layoutnet = LayoutNet()
+    out = layoutnet(x)
+    print(out)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 class LayoutNet(nn.Module):
     def __init__(self):
         super(LayoutNet, self).__init__()
@@ -23,19 +201,19 @@ class LayoutNet(nn.Module):
         )
 
         self.enc4 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, padding=1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
         self.enc5 = nn.Sequential(
-            nn.Conv2d(256, 512, 3, padding=1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
         self.enc6 = nn.Sequential(
-            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
         self.enc7 = nn.Sequential(
-            nn.Conv2d(1024, 2048, 3, padding=1),
+            nn.Conv2d(1024, 2048, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )        
 
@@ -96,10 +274,17 @@ class LayoutNet(nn.Module):
 
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.upsample = F.interpolate(scale_factor=2, mode='nearest')   # if below code runs ok, try using self.upsample
-        
+        #self.upsample = F.interpolate(scale_factor=2, mode='nearest')   # if below code runs ok, try using self.upsample
+        self.relu = nn.ReLU(inplace=True)
+
+        #ref0 = torch.reshape(2048 * 4 * 4)(encoder7_ds) #(pool7)
+        self.ref1 = nn.Linear(2048 * 4 * 4, 1024)
+        self.ref2 = nn.Linear(1024, 256)
+        self.ref3 = nn.Linear(256, 64)
+        self.roomtype = nn.Linear(64, 11)
         
     def forward(self, x):
+        print("x: ", x.shape)
         ## ENCODER
         encoder1 = self.enc1(x)
         encoder1_ds = self.pool(encoder1)  # ds ==> downsample
@@ -113,102 +298,109 @@ class LayoutNet(nn.Module):
         encoder5_ds = self.pool(encoder5) # pool5
         encoder6 = self.enc6(encoder5_ds) # conv6_relu 
         encoder6_ds = self.pool(encoder6) # pool6
+        print('pool6 ', encoder6_ds.shape)
         encoder7 = self.enc7(encoder6_ds) # conv7_relu 
         encoder7_ds = self.pool(encoder7) # pool7
+        print('pool7 shape: ', encoder7_ds.shape)
 
 
         ## DECODER
         
         unpool00 = F.interpolate(encoder7_ds,  # AKA pool7
                                     scale_factor=2, mode='nearest')
+        #print(unpool00.shape)
+        deconv00_relu = self.dec1(unpool00)
         decoder1 = torch.cat([
-            self.dec1(unpool00),  #deconv00_relu
+            deconv00_relu,  #deconv00_relu
             encoder6_ds  # AKA pool6
-            ]) # unpool0_
-        decoder2 = torch.cat([
-            self.dec2(F.interpolate(decoder1, #unpool0_
+            ],  dim=1) # unpool0_
+        print('deconv00_relu ', self.dec1(unpool00).shape)
+        print('decoder1 shape: ', decoder1.shape)
+        print("upsample: ", F.interpolate(decoder1, #unpool0_
+                                    scale_factor=2, mode='nearest').shape)
+        deconv0_relu = self.dec2(F.interpolate(decoder1, #unpool0_
                                     scale_factor=2, mode='nearest')
-                     ), #deconv0_relu 
+                     )
+        decoder2 = torch.cat([
+            deconv0_relu, #deconv0_relu 
             encoder5_ds # pool5
-            ]) #unpool1_ 
+            ],  dim=1) #unpool1_ 
         decoder3 = torch.cat([
             self.dec3(F.interpolate(decoder2, #unpool1_
                                     scale_factor=2, mode='nearest')
                      ), #deconv1_relu 
             encoder4_ds # pool4
-        ]) #unpool2_ 
+        ],  dim=1) #unpool2_ 
         decoder4 = torch.cat([
             self.dec4(F.interpolate(decoder3, #unpool2_
                                     scale_factor=2, mode='nearest')
                      ), #deconv2_relu 
             encoder3_ds # pool3
-        ]) #unpool3_ 
+        ],  dim=1) #unpool3_ 
         decoder5 = torch.cat([
             self.dec5(F.interpolate(decoder4, #unpool3_
                                     scale_factor=2, mode='nearest'
                                    )
                       ), #deconv3_relu 
             encoder2_ds # pool2
-        ]) #unpool4_ 
+        ],  dim=1) #unpool4_ 
         decoder6 = torch.cat([
             self.dec6(F.interpolate(decoder5, #unpool4_
                                     scale_factor=2, mode='nearest'
                                    )
                       ), #deconv4_relu
             encoder1_ds # pool1
-        ])  #unpool5_ 
+        ],  dim=1)  #unpool5_ 
         unpool5 = F.interpolate(decoder6, scale_factor=2, mode='nearest')
-        decoder7 = deconv6_sf = self.dec7(unpool5)
+        decoder6_sigmoid = self.dec7(unpool5)
         
         
         ## JOINT
         
-        joint1 =torch.cat([self.dec1(unpool00), #deconv00_relu_c 
+        joint1 =torch.cat([
+            self.dec1(unpool00), #deconv00_relu_c 
                            decoder1, # unpool0_ 
-                          ]) #unpool0_c
-        joint2  = torch.cat([self.jointdec2(F.interpolate(joint1, scale_factor=2, mode='nearest')),
+                          ],  dim=1) #unpool0_c
+        print(joint1.shape)
+        joint2  = torch.cat([
+            self.jointdec2(F.interpolate(joint1, scale_factor=2, mode='nearest')),
                                 decoder2, #unpool1_
-                               ]) #unpool1_c
-        joint3  = torch.cat([self.jointdec3(F.interpolate(joint2, scale_factor=2, mode='nearest')),
+                               ],  dim=1) #unpool1_c
+        joint3  = torch.cat([
+            self.jointdec3(F.interpolate(joint2, scale_factor=2, mode='nearest')),
                                 decoder3, #unpool2_
-                               ]) #unpool2_c
-        joint4  = torch.cat([self.jointdec4(F.interpolate(joint3, scale_factor=2, mode='nearest')),
+                               ],  dim=1) #unpool2_c
+        joint4  = torch.cat([
+            self.jointdec4(F.interpolate(joint3, scale_factor=2, mode='nearest')),
                                 decoder4, #unpool3_
-                               ]) #unpool3_c
-        joint5  = torch.cat([self.jointdec5(F.interpolate(joint4, scale_factor=2, mode='nearest')),
+                               ],  dim=1) #unpool3_c
+        joint5  = torch.cat([
+            self.jointdec5(F.interpolate(joint4, scale_factor=2, mode='nearest')),
                                 decoder5, #unpool4_
-                               ]) #unpool4_c
+                               ],  dim=1) #unpool4_c
         joint6  = torch.cat([
-            self.jointdec6(F.interpolate(joint4, scale_factor=2, mode='nearest')),
+            self.jointdec6(F.interpolate(joint5, scale_factor=2, mode='nearest')),
                                 decoder6, #unpool5_
-                               ]) #unpool5_c
-
-        joint2 = self.dec2(torch.cat([F.interpolate(encoder2, 2), decoder2]))
-        joint3 = self.dec3(torch.cat([F.interpolate(encoder3, 2), decoder3]))
-        joint4 = self.dec4(torch.cat([F.interpolate(encoder4, 2), decoder4]))
-        joint5 = self.dec5(torch.cat([F.interpolate(encoder5, 2), decoder5]))
-        joint6 = sigmoid(joint5)
+                               ],  dim=1) #unpool5_c
         
-'''
-unpool00 = nn.SpatialUpSamplingNearest(2)(pool7)
-deconv00 = nn.SpatialConvolution(2048,1024,3,3,1,1,1,1)(unpool00)
-deconv00_relu = nn.ReLU(true)(deconv00)
-'''
+        
+        decoder6_sigmoid_corners = self.jointdec7(F.interpolate(joint6, scale_factor=2, mode='nearest'))
+        
+
         ## REFINEMENT
 
-        ref0 = nn.reshape(2048 * 4 * 4) #(pool7)
-        ref1 = nn.Linear(2048 * 4 * 4, 1024) #(ref0)
-        ref1_relu = nn.ReLU(inplace=True) #(ref1)
-        ref2 = nn.Linear(1024, 256) #(ref1_relu)
-        ref2_relu = nn.ReLU(inplace=True) #(ref2)
-        ref3 = nn.Linear(256, 64) #(ref2_relu)
-        ref3_relu = nn.ReLU(inplace=True) #(ref3)
-        roomtype = nn.Linear(64, 11) #(ref3_relu)
+#        ref0 = encoder7_ds.flatten() #torch.reshape(2048 * 4 * 4)(encoder7_ds) #(pool7)
+        ref1 = self.ref1(encoder7_ds.view(-1, 2048 * 4 * 4))
+        print('ref1 ', ref1.shape)
+        ref1_relu = self.relu(ref1)
+        ref2 = self.ref2(ref1_relu)
+        ref2_relu = self.relu(ref2)
+        ref3 = self.ref3(ref2_relu)
+        ref3_relu = self.relu(ref3)
+        roomtype = self.roomtype(ref3_relu)
 
 
         return decoder6_sigmoid, decoder6_sigmoid_corners, roomtype
-
-
 
 
 
@@ -273,6 +465,7 @@ class Decoder(nn.Module):
             f_last = conv(f_last)
             f_last = torch.cat([f_last, f], dim=1)
             conv_out.append(f_last)
+            #print('f_last shape:', f_last.shape)
         conv_out.append(self.last_conv(F.interpolate(
             f_last, scale_factor=2, mode='nearest')))
         return conv_out
@@ -302,16 +495,26 @@ if __name__ == '__main__':
     encoder = Encoder()
     edg_decoder = Decoder(skip_num=2, out_planes=3)
     cor_decoder = Decoder(skip_num=3, out_planes=1)
+    my_layoutnet = LayoutNet()
 
     with torch.no_grad():
-        x = torch.rand(2, 6, 512, 1024)
-        en_list = encoder(x)
+        x_orig = torch.rand(2, 6, 512, 1024)
+        x = torch.rand(2, 3, 512, 512)
+        en_list = encoder(x_orig)
         edg_de_list = edg_decoder(en_list[::-1])
         cor_de_list = cor_decoder(en_list[-1:] + edg_de_list[:-1])
-
+        my_layoutnet_outputs = [decoder6_sigmoid, decoder6_sigmoid_corners, roomtype
+                               ] = my_layoutnet(x)
+    '''
     for f in en_list:
         print('encoder', f.size())
     for f in edg_de_list:
         print('edg_decoder', f.size())
     for f in cor_de_list:
         print('cor_decoder', f.size())
+    '''
+    for f in my_layoutnet_outputs:
+        print('my_layoutnet', f.size())
+
+"""
+
--- visual_persp.py
+++ visual_persp.py
@@ -2,15 +2,16 @@ import os
 import glob
 import argparse
 import numpy as np
+import torchfile
 from PIL import Image
 
 import torch
-from model import Encoder, Decoder
+from model import LayoutNet
 from utils_eval import augment, augment_undo
 from pano import get_ini_cor, draw_boundary_from_cor_id
 from pano_opt import optimize_cor_id
 
-
+'''
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 # Model related arguments
 parser.add_argument('--path_prefix', default='ckpt/pre',
@@ -42,94 +43,97 @@ parser.add_argument('--post_optimization', action='store_true',
                     help='whether to performe post gd optimization')
 args = parser.parse_args()
 device = torch.device(args.device)
-
-# Check input arguments validation
-for path in glob.glob(args.img_glob):
-    assert os.path.isfile(path), '%s not found' % path
-for path in glob.glob(args.line_glob):
-    assert os.path.isfile(path), '%s not found' % path
-assert os.path.isdir(args.output_dir), '%s is not a directory' % args.output_dir
-for rotate in args.rotate:
-    assert 0 <= rotate and rotate <= 1, 'elements in --rotate should in [0, 1]'
+'''
 
 
-# Prepare model
-encoder = Encoder(in_planes=3).to(device)
-edg_decoder = Decoder(skip_num=2, out_planes=3).to(device)
-cor_decoder = Decoder(skip_num=3, out_planes=1).to(device)
-refinement = 
-"""nn.Reshape(2048*4*4)(pool7)		local deconv6_comb = nn.JoinTable(2)({deconv6_sf, deconv6_sf_c})
-local ref1 = nn.Linear(2048*4*4, 1024)(ref0)		local conv_r1 = nn.SpatialConvolution(4,8,3,3,1,1,1,1)(deconv6_comb)
-"""
-encoder.load_state_dict(torch.load('%s_encoder.pth' % args.path_prefix))
-edg_decoder.load_state_dict(torch.load('%s_edg_decoder.pth' % args.path_prefix))
-cor_decoder.load_state_dict(torch.load('%s_cor_decoder.pth' % args.path_prefix))
+def visual_persp(img_glob, output_dir, device=torch.device("cpu"), path_prefix='ckpt/pre',flip=True,  rotate=[], d1=21, d2=3):
+
+    # Check input arguments validation
+    for path in glob.glob(img_glob):
+        assert os.path.isfile(path), '%s not found' % path
+    assert os.path.isdir(output_dir), '%s is not a directory' % output_dir
+    for rotate in rotate:
+        assert 0 <= rotate and rotate <= 1, 'elements in --rotate should in [0, 1]'
 
 
-# Load path to visualization
-img_paths = sorted(glob.glob(args.img_glob))
-line_paths = sorted(glob.glob(args.line_glob))
-assert len(img_paths) == len(line_paths), '# of input mismatch for each channels'
+    # Prepare model
+    layoutnet = LayoutNet().to(device)
+    torch_pretrained = torchfile.load('/home/jupyter/Shapes.ai/pytorch-layoutnet/ckpt/perspfull_lsun_type_pretrained.t7')
+    total_parameter =0
+    for p in layoutnet.parameters():
+        total_parameter += np.prod(p.size())
+
+    torch.save(layoutnet.state_dict(), '/ckpt/persp_pretrained.pth')
+        
+    # Load path to visualization
+    img_paths = sorted(glob.glob(img_glob))
 
 
-# Process each input
-for i_path, l_path in zip(img_paths, line_paths):
-    print('img  path:', i_path)
-    print('line path:', l_path)
+    # Process each input
+    for i_path in img_paths:
+        print('img  path:', i_path)
 
-    # Load and cat input images
-    i_img = np.array(Image.open(i_path), np.float32) / 255
-    l_img = np.array(Image.open(l_path), np.float32) / 255
-    x_img = np.concatenate([
-        i_img.transpose([2, 0, 1]),
-        l_img.transpose([2, 0, 1])], axis=0)
+        # Load and cat input images
+        i_img = np.array(Image.open(i_path).resize((512,512)), np.float32) / 255
+        #l_img = np.array(Image.open(l_path), np.float32) / 255
+        x_img = i_img.transpose([2, 0, 1])
+        print('x_img shape: ', x_img.shape)
+        
+        # Augment data
+        x_imgs_augmented, aug_type = augment(x_img, flip, rotate)
 
-    # Augment data
-    x_imgs_augmented, aug_type = augment(x_img, args.flip, args.rotate)
+        # Feedforward and extract output images
+        with torch.no_grad():
+            x = torch.FloatTensor(x_imgs_augmented).to(device)
+            print('x shape: ', x.shape)
+            deconv6_sf, deconv6_sf_c, ref4 = layoutnet(x) 
+            #edg_de_list = edg_decoder(en_list[::-1])
+            #cor_de_list = cor_decoder(en_list[-1:] + edg_de_list[:-1])
 
-    # Feedforward and extract output images
-    with torch.no_grad():
-        x = torch.FloatTensor(x_imgs_augmented).to(device)
-        en_list = encoder(x)
-        edg_de_list = edg_decoder(en_list[::-1])
-        cor_de_list = cor_decoder(en_list[-1:] + edg_de_list[:-1])
+            edg_tensor = deconv6_sf
+            cor_tensor = deconv6_sf_c
+            roomtype_tensor = ref4
 
-        edg_tensor = torch.sigmoid(edg_de_list[-1])
-        cor_tensor = torch.sigmoid(cor_de_list[-1])
+            # Recover the effect from augmentation
+            edg_img = augment_undo(edg_tensor.cpu().numpy(), aug_type)
+            cor_img = augment_undo(cor_tensor.cpu().numpy(), aug_type)
 
-        # Recover the effect from augmentation
-        edg_img = augment_undo(edg_tensor.cpu().numpy(), aug_type)
-        cor_img = augment_undo(cor_tensor.cpu().numpy(), aug_type)
 
-    # Merge all results from augmentation
-    edgmap = edg_img.transpose([0, 2, 3, 1]).mean(0).copy()
-    cormap = cor_img.transpose([0, 2, 3, 1]).mean(0)[..., 0].copy()
+        # Merge all results from augmentation
+        edgmap = edg_img.transpose([0, 2, 3, 1]).mean(0).copy()
+        cormap = cor_img.transpose([0, 2, 3, 1]).mean(0)[..., 0].copy()
 
-    # Post processing to extract layout
-    cor_id = get_ini_cor(cormap, args.d1, args.d2)
-    if args.post_optimization:
-        cor_id = optimize_cor_id(cor_id, edgmap, cormap,
-                                 num_iters=100, verbose=False)
+        # Post processing to extract layout
+        cor_id = get_ini_cor(cormap, d1, d2)
+        #if post_optimization:
+        #    cor_id = optimize_cor_id(cor_id, edgmap, cormap,
+        #                             num_iters=100, verbose=False)
 
-    # Draw extracted layout on source image
-    bon_img = draw_boundary_from_cor_id(cor_id.copy(), i_img * 255)
+        # Draw extracted layout on source image
+        bon_img = draw_boundary_from_cor_id(cor_id.copy(), i_img * 255)
 
-    # Composite all result in one image
-    all_in_one = 0.3 * edgmap + 0.3 * cormap[..., None] + 0.4 * i_img
-    all_in_one = draw_boundary_from_cor_id(cor_id.copy(), all_in_one * 255)
+        # Composite all result in one image
+        all_in_one = 0.3 * edgmap + 0.3 * cormap[..., None] + 0.4 * i_img
+        all_in_one = draw_boundary_from_cor_id(cor_id.copy(), all_in_one * 255)
 
-    # Dump results
-    basename = os.path.splitext(os.path.basename(i_path))[0]
-    path_edg = os.path.join(args.output_dir, '%s_edg.png' % basename)
-    path_cor = os.path.join(args.output_dir, '%s_cor.png' % basename)
-    path_bon = os.path.join(args.output_dir, '%s_bon.png' % basename)
-    path_all_in_one = os.path.join(args.output_dir, '%s_all.png' % basename)
-    path_cor_id = os.path.join(args.output_dir, '%s_cor_id.txt' % basename)
+        # Dump results
+        basename = os.path.splitext(os.path.basename(i_path))[0]
+        path_edg = os.path.join(output_dir, '%s_edg.png' % basename)
+        path_cor = os.path.join(output_dir, '%s_cor.png' % basename)
+        path_bon = os.path.join(output_dir, '%s_bon.png' % basename)
+        path_all_in_one = os.path.join(output_dir, '%s_all.png' % basename)
+        path_cor_id = os.path.join(output_dir, '%s_cor_id.txt' % basename)
 
-    Image.fromarray((edgmap * 255).astype(np.uint8)).save(path_edg)
-    Image.fromarray((cormap * 255).astype(np.uint8)).save(path_cor)
-    Image.fromarray(bon_img).save(path_bon)
-    Image.fromarray(all_in_one).save(path_all_in_one)
-    with open(path_cor_id, 'w') as f:
-        for x, y in cor_id:
-            f.write('%.6f %.6f\n' % (x, y))
+        Image.fromarray((edgmap * 255).astype(np.uint8)).save(path_edg)
+        Image.fromarray((cormap * 255).astype(np.uint8)).save(path_cor)
+        Image.fromarray(bon_img).save(path_bon)
+        Image.fromarray(all_in_one).save(path_all_in_one)
+        with open(path_cor_id, 'w') as f:
+            for x, y in cor_id:
+                f.write('%.6f %.6f\n' % (x, y))
+
+            
+if __name__ == "__main__":
+    img_glob='assert/ADE_val_00000064.jpg'
+    output_dir='assert/output/'
+    visual_persp(img_glob, output_dir,)
